16a3e12e6f20c0699192bf29fa434615de094c99
Joanna Ng <joanna.ng@bmail.edu>
2017-04-24 16:10:38 -0700

    started library page

--- app/src/main/java/com/example/wingsoffreedom/readalive/CreateProfile.java
+++ app/src/main/java/com/example/wingsoffreedom/readalive/CreateProfile.java
@@ -1,7 +1,10 @@
 package com.example.wingsoffreedom.readalive;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateProfile extends AppCompatActivity {
 
@@ -11,6 +14,16 @@ public class CreateProfile extends AppCompatActivity {
         setContentView(R.layout.activity_create_profile);
 
 
+        Button cancel = (Button) findViewById(R.id.cancel_create);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateProfile.this, ChooseProfile.class);
+                startActivity(intent);
+            }
+
+        });
 
     }
 }
