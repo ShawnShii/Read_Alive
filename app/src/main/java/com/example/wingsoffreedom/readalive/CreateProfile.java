0aaef94648c2bdaa80273479cf7b993e9d3567aa
Joanna Ng <joanna.ng@bmail.edu>
2017-04-24 17:04:19 -0700

    create profile

--- app/src/main/java/com/example/wingsoffreedom/readalive/CreateProfile.java
+++ app/src/main/java/com/example/wingsoffreedom/readalive/CreateProfile.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateProfile extends AppCompatActivity {
 
@@ -13,13 +14,26 @@ public class CreateProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
 
-
         Button cancel = (Button) findViewById(R.id.cancel_create);
+        Button create = (Button) findViewById(R.id.continue_create);
+
+        final EditText nameInput = (EditText) findViewById(R.id.name_input);
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(CreateProfile.this, ChooseProfile.class);
+                startActivity(intent);
+            }
+
+        });
+
+        create.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateProfile.this, ReadAlive.class);
+                intent.putExtra("name", (String) nameInput.getText().toString());
                 startActivity(intent);
             }
 
--- app/src/main/java/com/example/wingsoffreedom/readalive/TabProfile.java
+++ app/src/main/java/com/example/wingsoffreedom/readalive/TabProfile.java
@@ -9,12 +9,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import static android.R.attr.name;
 
 public class TabProfile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_profile, container, false);
+        Bundle extras = getActivity().getIntent().getExtras();
+        String name = extras.getString("name");
+        TextView userName = (TextView) rootView.findViewById(R.id.profileName);
+        userName.setText(name);
+
         return rootView;
     }
 }
