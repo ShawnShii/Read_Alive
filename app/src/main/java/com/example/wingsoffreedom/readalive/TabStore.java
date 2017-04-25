0f610c12abd86a3316ccc3d5c5792aeedd1a1bd4
Shawn Shi <shawnshi101@berkeley.edu>
2017-04-24 20:09:36 -0700

    store page efforts were made

--- app/src/main/java/com/example/wingsoffreedom/readalive/TabStore.java
+++ app/src/main/java/com/example/wingsoffreedom/readalive/TabStore.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
 
 /**
  * Created by WingsofFreedom on 4/24/17.
