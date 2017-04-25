1600e534864ed0cb8cd2521d476d72ee61901a18
Joanna Ng <joanna.ng@bmail.edu>
2017-04-24 20:28:03 -0700

    added ahopping cart

--- app/src/main/java/com/example/wingsoffreedom/readalive/TabStore.java
+++ app/src/main/java/com/example/wingsoffreedom/readalive/TabStore.java
@@ -1,11 +1,13 @@
 package com.example.wingsoffreedom.readalive;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -18,6 +20,18 @@ public class TabStore extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_store, container, false);
+
+        Button cart = (Button) rootView.findViewById(R.id.button_cart);
+
+        cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ShoppingCart.class);
+                startActivity(intent);
+            }
+
+        });
+
         return rootView;
     }
 }
