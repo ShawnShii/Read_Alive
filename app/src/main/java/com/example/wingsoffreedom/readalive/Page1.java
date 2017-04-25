ff9cb4b22c87f43cac3da953c1e8d0ca8c080bec
Joanna Ng <joanna.ng@bmail.edu>
2017-04-24 19:08:59 -0700

    added image

---
+++ app/src/main/java/com/example/wingsoffreedom/readalive/Page1.java
@@ -0,0 +1,24 @@
+package com.example.wingsoffreedom.readalive;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+/**
+ * Created by WingsofFreedom on 4/24/17.
+ */
+
+public class Page1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.page_1, container, false);
+
+        return rootView;
+    }
+}
