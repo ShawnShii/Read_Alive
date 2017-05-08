f2e7fd436e9b3491254d8d6e3c6d6bad03add87d
Joanna Ng <joanna.ng@bmail.edu>
2017-05-08 10:20:29 -0700

    added trophy popups

--- app/src/main/java/com/example/wingsoffreedom/readalive/TabProfile.java
+++ app/src/main/java/com/example/wingsoffreedom/readalive/TabProfile.java
@@ -7,6 +7,7 @@ package com.example.wingsoffreedom.readalive;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,21 +41,6 @@ public class TabProfile extends Fragment {
             profile_avatar.setImageResource(R.drawable.monkey_avatar);
         }
 
-
-        if (name.equals("Ellen Smith")) {
-            ImageView gold = (ImageView) rootView.findViewById(R.id.gold_earned);
-            ImageView silver = (ImageView) rootView.findViewById(R.id.silver_earned);
-            ImageView bronze = (ImageView) rootView.findViewById(R.id.bronze_earned);
-            ImageView bronze_2 = (ImageView) rootView.findViewById(R.id.bronze_earned_2);
-            ImageView bronze_3 = (ImageView) rootView.findViewById(R.id.bronze_earned_3);
-
-            gold.setImageResource(R.drawable.gold);
-            silver.setImageResource(R.drawable.silver);
-            bronze.setImageResource(R.drawable.bronze);
-            bronze_2.setImageResource(R.drawable.bronze);
-            bronze_3.setImageResource(R.drawable.bronze);
-        }
-
         TextView userChange = (TextView) rootView.findViewById(R.id.user_change);
 
         userChange.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +51,124 @@ public class TabProfile extends Fragment {
             }
 
         });
+
+        ImageView gold = (ImageView) rootView.findViewById(R.id.trophy_r_2_hr);
+        ImageView silver = (ImageView) rootView.findViewById(R.id.trophy_r_10_days);
+        ImageView bronze = (ImageView) rootView.findViewById(R.id.trophy_finish_pic);
+
+        ImageView gold_2 = (ImageView) rootView.findViewById(R.id.trophy_r_10_week);
+        ImageView silver_2 = (ImageView) rootView.findViewById(R.id.trophy_finish_5_pic);
+        ImageView bronze_2 = (ImageView) rootView.findViewById(R.id.trophy_r_1_hr_week);
+
+        ImageView gold_3 = (ImageView) rootView.findViewById(R.id.trophy_finish_15_pic);
+        ImageView silver_3 = (ImageView) rootView.findViewById(R.id.trophy_10_cat);
+        ImageView bronze_3 = (ImageView) rootView.findViewById(R.id.trophy_r_1_hr_day);
+
+        if (name.equals("Ellen Smith")) {
+            gold.setImageResource(R.drawable.gold);
+            silver.setImageResource(R.drawable.silver);
+            bronze.setImageResource(R.drawable.bronze);
+            bronze_2.setImageResource(R.drawable.bronze);
+            bronze_3.setImageResource(R.drawable.bronze);
+        }
+
+        gold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment trophy_1 = new trophy_1();
+                FragmentTransaction trophyFrag = getChildFragmentManager().beginTransaction();
+                trophyFrag.add(R.id.test, trophy_1, "trophy_1").commit();
+            }
+
+        });
+
+        silver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment trophy_2 = new trophy_2();
+                FragmentTransaction trophyFrag = getChildFragmentManager().beginTransaction();
+                trophyFrag.add(R.id.test, trophy_2, "trophy_2").commit();
+            }
+
+        });
+
+        bronze.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment trophy_3 = new trophy_3();
+                FragmentTransaction trophyFrag = getChildFragmentManager().beginTransaction();
+                trophyFrag.add(R.id.test, trophy_3, "trophy_3").commit();
+
+            }
+
+        });
+
+        gold_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment trophy_4 = new trophy_4();
+                FragmentTransaction trophyFrag = getChildFragmentManager().beginTransaction();
+                trophyFrag.add(R.id.test, trophy_4, "trophy_4").commit();
+
+            }
+
+        });
+
+        silver_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment trophy_5 = new trophy_5();
+                FragmentTransaction trophyFrag = getChildFragmentManager().beginTransaction();
+                trophyFrag.add(R.id.test, trophy_5, "trophy_5").commit();
+
+            }
+
+        });
+
+        bronze_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment trophy_6 = new trophy_6();
+                FragmentTransaction trophyFrag = getChildFragmentManager().beginTransaction();
+                trophyFrag.add(R.id.test, trophy_6, "trophy_6").commit();
+            }
+
+        });
+
+
+        gold_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment trophy_7 = new trophy_7();
+                FragmentTransaction trophyFrag = getChildFragmentManager().beginTransaction();
+                trophyFrag.add(R.id.test, trophy_7, "trophy_7").commit();
+
+            }
+
+        });
+
+        silver_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment trophy_8 = new trophy_8();
+                FragmentTransaction trophyFrag = getChildFragmentManager().beginTransaction();
+                trophyFrag.add(R.id.test, trophy_8, "trophy_8").commit();
+
+            }
+
+        });
+
+        bronze_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment trophy_9 = new trophy_9();
+                FragmentTransaction trophyFrag = getChildFragmentManager().beginTransaction();
+                trophyFrag.add(R.id.test, trophy_9, "trophy_9").commit();
+
+            }
+
+        });
+
 
         return rootView;
     }
---
+++ app/src/main/java/com/example/wingsoffreedom/readalive/trophy_8.java
@@ -0,0 +1,44 @@
+package com.example.wingsoffreedom.readalive;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class trophy_8 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        final View rootView = inflater.inflate(R.layout.fragment_trophy_8, container, false);
+
+        final TextView close = (TextView) rootView.findViewById(R.id.closeTrophy);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView trophy_img = (ImageView) rootView.findViewById(R.id.trophy_10_cat);
+                TextView trophy_txt = (TextView) rootView.findViewById(R.id.trophy_8_txt);
+                FrameLayout trophy_layout = (FrameLayout) rootView.findViewById(R.id.trophy_8_popup);
+
+                trophy_img.setImageResource(0);
+                trophy_txt.setText("");
+                close.setText("");
+                trophy_layout.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        return rootView;
+    }
+}
